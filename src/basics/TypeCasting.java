f224792d36d8b314674aca34890d35d970060551
Rajashekhar Perala <peralarajashekhar2004@gmail.com>
2026-09-04 16:03:15 +0530

    Practice java variables and type casting

---
+++ src/basics/TypeCasting.java
@@ -0,0 +1,19 @@
+package basics;
+
+public class TypeCasting {
+
+    public static void main(String[] args) {
+
+        int number = 100;
+
+        double decimalNumber = number;
+
+        System.out.println(decimalNumber);
+
+        double price = 99.99;
+
+        int roundedPrice = (int) price;
+
+        System.out.println(roundedPrice);
+    }
+}
---
+++ src/basics/Variables.java
@@ -0,0 +1,19 @@
+package basics;
+
+public class Variables {
+
+    public static void main(String[] args) {
+
+        int age = 22;
+        double salary = 35000.50;
+        char grade = 'A';
+        boolean employed = false;
+        String name = "Raju";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Employed: " + employed);
+    }
+}
